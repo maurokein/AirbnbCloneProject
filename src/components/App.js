56f5395d8135df37b37dbe7ae0932bed5f44e0b8
maurokein <maurokein1990@gmail.com>
2022-01-23 09:40:47 -0300

    passing data from app to component  as props

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Card from './Card';
+import Image from '../images/swimmerimage.png'
 
 export default function App(){
     return(
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <Card
+                img = {Image}
+                punctuation = {5.0}
+                counter = {6}
+                country = "USA"
+                title = "Life lessons with Katie Zaferes"
+                price = {136}
+            />
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,20 @@ import Image from '../images/swimmerimage.png'
 import Icon from '../images/staricon.png'
 import Ellipse from '../images/ellipseicon.png'
 
-export default function Card(){
+export default function Card(props){
     return(
         <section className='card'>
-            <img src={Image} alt='smilingswimmer' className='card--image'/>
+            <img src={props.img} alt='smilingswimmer' className='card--image'/>
             <div className='card--reviews'>
                 <img src={Icon} alt='staricon' className='card--reviews__star'/>
-                <span className='card--reviews__punctuation'>5.0</span>
-                <span className='card--reviews__counter'>(6)</span>
+                <span className='card--reviews__punctuation'>{props.punctuation}</span>
+                <span className='card--reviews__counter'>({props.counter})</span>
                 <span><img src={Ellipse} alt='ellipse' className='card--reviews__ellipse'/></span>
-                <span className='card--reviews__country'>USA</span>
+                <span className='card--reviews__country'>{props.country}</span>
             </div>
-            <span className='card--title'>Life lessons with Katie Zaferes</span>
+            <span className='card--title'>{props.title}</span>
             <br/>
-            <span className='card--price'>From $136</span>
+            <span className='card--price'>From ${props.price}</span>
             <span className='card--person'> / person</span>
         </section>
     )
