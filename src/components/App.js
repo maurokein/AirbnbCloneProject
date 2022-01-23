c49b98ff6402bd8214fc24273427f70658186023
maurokein <maurokein1990@gmail.com>
2022-01-23 17:56:32 -0300

    working with external data inside application

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Card from './Card';
-import Image from '../images/swimmerimage.png'
+import data from '../data';
+
+
 
 export default function App(){
+    const cards = data.map(card => 
+    <Card 
+        key = {card.id}
+        img = {card.coverImg}
+        punctuation = {card.stats.rating}
+        counter = {card.stats.reviewCount}
+        location = {card.location}
+        title = {card.title}
+        price = {card.price} />)
     return(
         <div>
             <Navbar />
             <Hero />
-            <Card
-                img = {Image}
-                punctuation = {5.0}
-                counter = {6}
-                country = "USA"
-                title = "Life lessons with Katie Zaferes"
-                price = {136}
-            />
+            <section className='cards--list'>
+                {cards}
+            </section>
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Image from '../images/swimmerimage.png'
 import Icon from '../images/staricon.png'
 import Ellipse from '../images/ellipseicon.png'
 
 export default function Card(props){
     return(
         <section className='card'>
-            <img src={props.img} alt='smilingswimmer' className='card--image'/>
+            <img src={props.img} alt='{props.img}' className='card--image'/>
             <div className='card--reviews'>
                 <img src={Icon} alt='staricon' className='card--reviews__star'/>
                 <span className='card--reviews__punctuation'>{props.punctuation}</span>
                 <span className='card--reviews__counter'>({props.counter})</span>
                 <span><img src={Ellipse} alt='ellipse' className='card--reviews__ellipse'/></span>
-                <span className='card--reviews__country'>{props.country}</span>
+                <span className='card--reviews__country'>{props.location}</span>
             </div>
             <span className='card--title'>{props.title}</span>
             <br/>
